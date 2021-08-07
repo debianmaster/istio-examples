33b327015564b2269c8a44b049983f6464b8fec2
chakra j <debianmaster@users.noreply.github.com>
2021-08-07 22:56:46 +0800

    Create create.sh

---
+++ tls-gateway/create.sh
@@ -0,0 +1,40 @@
+kubectl create secret tls cloud4-tls-cert --key=cert.key --cert=cert.cert  
+--
+kubectl apply -f - <<EOF
+apiVersion: networking.istio.io/v1alpha3
+kind: Gateway
+metadata:
+  name: httpbin-gateway
+spec:
+  selector:
+    istio: ingressgateway # use Istio default gateway implementation
+  servers:
+  - port:
+      number: 443
+      name: https
+      protocol: HTTPS
+    tls:
+      mode: SIMPLE
+      credentialName: cloud4-tls-cert  
+    hosts:
+    - "*"
+---
+apiVersion: networking.istio.io/v1alpha3
+kind: VirtualService
+metadata:
+  name: httpbin
+spec:
+  hosts:
+  - "cloud4.io"
+  gateways:
+  - httpbin-gateway
+  http:
+  - match:
+    - uri:
+        prefix: /
+    route:
+    - destination:
+        port:
+          number: 8080
+        host: wel
+EOF
